cfc6d3920d825894e6292b7437ff5e7d14a73ffc
Andres Fernando Velasco <af_velasco@misena.edu.co>
2022-06-14 17:55:10 -0500

    Ejercicio Formula 1

---
+++ src/EjercicioPOO.py
@@ -0,0 +1,14 @@
+'''
+Se trata de elaborar un programa Orientado a Objetos para simular una 
+carrera de hasta 4 carros. Cada carro tiene potencia inicial 10, sus cuatro 
+ruedas y dos farolas. La pista de 100 casillas es recorrida por cada carro
+utilizando para avanzar un dado especial cuyo número de caras corresponderá
+a la potencia del carro. A lo largo de la pista se deben distribuir
+aleatoriamente 2 huecos, 4 postes y 3 puntillas, un hueco hará que el carro
+quede con la mitad de su potencia actual, un poste quebrará una farola y
+se pierde un punto de potencia, una puntilla pincha una rueda perdiendo 2
+puntos de potencia (la potencia mínima es de 1), también en la pista se
+deben colocar 3 talleres que harán que el carro recupere todas sus
+características y toda su potencia.  Ganará el primer vehículo en llegar
+a la meta (casilla 100)
+'''
